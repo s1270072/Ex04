4a0cbcfa477d4535b4df15c3210123b1ced2a8d0
Daiki Sakai <s1270072@u-aizu.ac.jp>
2020-08-05 17:59:34 +0900

    Victory message added

--- coin.c
+++ coin.c
@@ -4,7 +4,12 @@
 
 int main() {
 	int rnd,cntH=0,cntT=0;
+	//char name[15];
 	srand((unsigned int)time(NULL));
+
+	/*printf("Who are you?\n>");
+	scanf_s("%s", name,15);
+	printf("Hello, %s\n", name);*/
 
 	printf("Tossing a coin...\n");
 	for (int i = 0;i < 3;i++) {
@@ -20,6 +25,8 @@ int main() {
 		}
 	}
 	printf("Heads: %d, Tails: %d\n",cntH,cntT);
+	if (cntH > cntT) printf("You won\n");
+	else printf("You lost\n");
 
 	return 0;
 }
